f1d4cddb1eb070e7750ddb07e6da8fd9e6768f66
abhinavsalar18 <abhinav.2021ca006@mnnit.ac.in>
2024-01-03 00:30:40 +0530

    UI updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 
 import Chatboat from "./components/Chatboat"
+const style = {
+  marginTop: "10px", 
+  fontSize: "25px", 
+  color: "rgb(15, 206, 228)", 
+  display: "flex", 
+  flexDirection: "column", 
+  justifyContent: "center", 
+  alignContent: "center", 
+  alignItems: "center"
+}; 
+
 function App() {
   return (
     <div className="App">
+      <div style={style} className="">
+        <span>Hi!👋 there!</span>
+        <span>I'm a chatboat</span>
+        <span >Ask me about <span style={{fontWeight: "bold", filter: "drop-shadow(2px 2px 2px gray)"}}>SciAstra</span></span>
+      </div>
       <Chatboat />
     </div>
   );
